a7d504b766eeee2af53bbc48f2da99217d828d92
soerengebbert <soerengebbert@16fbe261-6e97-5eb1-f61f-830664b603fa>
2011-03-27 21:47:32 +0000

    Added WFIS documentation. Added monte carlo variation WFIS test.

--- trunk/Filtering/Testing/Python/vtkTAG2EModelMonteCarloVariationAnalyserTests.py
+++ trunk/Filtering/Testing/Python/vtkTAG2EModelMonteCarloVariationAnalyserTests.py
@@ -344,7 +344,7 @@ class vtkTAG2EModelMonteCarloVariationAnalyserTestsComplex(unittest.TestCase):
         root.AddNestedElement(var2)
         root.AddNestedElement(var3)
         
-        self.ddd.SetFileName("/tmp/MCCompexSimpleDataDistributionDescription1.xml")
+        self.ddd.SetFileName("/tmp/MCLRComplexSimpleDataDistributionDescription1.xml")
         self.ddd.GetXMLRoot().DeepCopy(root)
         self.ddd.Write()
                 
@@ -365,7 +365,7 @@ class vtkTAG2EModelMonteCarloVariationAnalyserTestsComplex(unittest.TestCase):
         writer = vtkXMLPolyDataWriter()
         
         for i in range(1):
-            writer.SetFileName("/tmp/TAG2EMonteCarloComplexTest_1_" + str(i) + ".vtp")
+            writer.SetFileName("/tmp/TAG2EMonteCarloLRComplexTest_1_" + str(i) + ".vtp")
             writer.SetInput(output.GetTimeStep(i))
             writer.Write()
             
@@ -376,7 +376,216 @@ class vtkTAG2EModelMonteCarloVariationAnalyserTestsComplex(unittest.TestCase):
         self.riface.AssignVTKDataArrayToRVariable(output.GetFieldData().GetArray(self.Model.GetResultArrayName()), self.Model.GetResultArrayName())
         
         # Save the workspace for testing
-        script = "save(list = ls(all=TRUE), file = \"/home/soeren/MonteCarloTest\")"
+        script = "save(list = ls(all=TRUE), file = \"/home/soeren/MonteCarloTestLR\")"
+        print script
+        self.riface.EvalRscript(script, True)
+    
+# test a weighted fuzzy inference scheme with monte carlo analysis
+class vtkTAG2EModelMonteCarloVariationAnalyserTestsWFIS(unittest.TestCase):
+       
+    def setUp(self):
+        
+        ########################################################################
+        # Set up the model parameter
+        # We build a weighted fuzzy inference scheme
+        
+        # Start the interface
+        self.riface = vtkRInterfaceSpatial()
+         
+        root  = vtk.vtkXMLDataElement()
+        
+        fuzzyRoot = vtkXMLDataElement()
+        fss1 = vtkXMLDataElement()
+        fss2 = vtkXMLDataElement()
+        fs1 = vtkXMLDataElement()
+        fs2 = vtkXMLDataElement()
+        fs3 = vtkXMLDataElement()
+        tr1 = vtkXMLDataElement()
+        tr2 = vtkXMLDataElement()
+        tr3 = vtkXMLDataElement()
+        resp = vtkXMLDataElement()
+        
+        weight = vtkXMLDataElement()
+        
+# Triangular test shape layout
+# ____        ____
+#     \  /\  /
+#      \/  \/
+#      /\  /\
+#     /  \/  \
+# 0  0.3 0.5 0.7  1
+#
+# - 1 - - 2 - - 3 - 
+#
+# 1: left = 2222 center = 0.3 right = 0.2
+# 2: left = 0.2  center = 0.5 right = 0.2
+# 3: left = 0.2  center = 0.7 right = 2222
+#       
+        
+        tr1.SetName("Triangular")
+        tr1.SetDoubleAttribute("center", 0.3)
+        tr1.SetDoubleAttribute("left",   2222)
+        tr1.SetDoubleAttribute("right",  0.2)
+        
+        tr2.SetName("Triangular")
+        tr2.SetDoubleAttribute("center", 0.5)
+        tr2.SetDoubleAttribute("left",   0.2)
+        tr2.SetDoubleAttribute("right",  0.2)
+        
+        tr3.SetName("Triangular")
+        tr3.SetDoubleAttribute("center",  0.7)
+        tr3.SetDoubleAttribute("left",    0.2)
+        tr3.SetDoubleAttribute("right",   2222)
+        
+        fs1.SetName("FuzzySet")
+        fs1.SetAttribute("type", "Triangular")
+        fs1.SetIntAttribute("priority", 0)
+        fs1.SetIntAttribute("const", 0)
+        fs1.SetAttribute("position", "left")
+        fs1.AddNestedElement(tr1)
+        
+        fs2.SetName("FuzzySet")
+        fs2.SetAttribute("type", "Triangular")
+        fs2.SetIntAttribute("priority", 0)
+        fs2.SetIntAttribute("const", 0)
+        fs2.SetAttribute("position", "intermediate")
+        fs2.AddNestedElement(tr2)
+        
+        fs3.SetName("FuzzySet")
+        fs3.SetAttribute("type", "Triangular")
+        fs3.SetIntAttribute("priority", 0)
+        fs3.SetIntAttribute("const", 0)
+        fs3.SetAttribute("position", "right")
+        fs3.AddNestedElement(tr3)
+        
+        # Two factors 
+        fss1.SetName("Factor")
+        fss1.SetIntAttribute("portId", 0)
+        fss1.SetAttribute("name", "pH")
+        fss1.SetDoubleAttribute("min", 0.0)
+        fss1.SetDoubleAttribute("max", 14.0)
+        fss1.AddNestedElement(fs1)
+        fss1.AddNestedElement(fs2)
+        fss1.AddNestedElement(fs3)
+        
+        fss2.SetName("Factor")
+        fss2.SetIntAttribute("portId", 0)
+        fss2.SetAttribute("name", "nmin")
+        fss2.SetDoubleAttribute("min", 0.0)
+        fss2.SetDoubleAttribute("max", 150)
+        fss2.AddNestedElement(fs1)
+        fss2.AddNestedElement(fs2)
+        fss2.AddNestedElement(fs3)
+        
+        resp.SetName("Responses")
+        resp.SetDoubleAttribute("min", 0.0)
+        resp.SetDoubleAttribute("max", 120.0)
+        
+        for i in range(9):
+            rval1 = vtkXMLDataElement()
+            rval1.SetName("Response")
+            rval1.SetIntAttribute("const", 0)
+            rval1.SetIntAttribute("sd", 1)
+            rval1.SetCharacterData(str(i*12), 6)
+            resp.AddNestedElement(rval1)
+                
+        fuzzyRoot.SetName("FuzzyInferenceScheme")
+        fuzzyRoot.AddNestedElement(fss1)
+        fuzzyRoot.AddNestedElement(fss2)
+        fuzzyRoot.AddNestedElement(resp)
+        
+        weight.SetName("Weight")
+        weight.SetAttribute("name", "grass")
+        weight.SetIntAttribute("active", 1)
+        weight.SetIntAttribute("const", 0)
+        weight.SetDoubleAttribute("min", 0)
+        weight.SetDoubleAttribute("max", 10)
+        weight.SetCharacterData("1.0", 5)
+        
+        root.SetName("WeightedFuzzyInferenceScheme")
+        root.SetAttribute("name", "N2OEmission_V20101111")
+        root.SetAttribute("xmlns", "http://tag2e.googlecode.com/files/WightedFuzzyInferenceScheme")
+        root.SetAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
+        root.SetAttribute("xsi:schemaLocation", "http://tag2e.googlecode.com/files/WeightedFuzzyInferenceScheme http://tag2e.googlecode.com/files/WeightedFuzzyInferenceScheme.xsd")
+        root.AddNestedElement(fuzzyRoot)
+        root.AddNestedElement(weight)
+        
+        fisc = vtkTAG2EFuzzyInferenceModelParameter()
+        fisc.SetFileName("/tmp/FuzzyInferenceScheme1.xml")
+        fisc.GetXMLRoot().DeepCopy(root)
+        
+        # Set up the model
+        self.Model = vtkTAG2EWeightedFuzzyInferenceModel()
+        self.Model.SetModelParameter(fisc)
+        
+    def test1(self):
+        
+        self.ddd = vtkTAG2EAbstractModelParameter()
+                
+        df1 = vtkXMLDataElement()
+        df1.SetName("norm")
+        df1.SetDoubleAttribute("mean", 75)
+        df1.SetDoubleAttribute("sd", 15)
+        
+        df2 = vtkXMLDataElement()
+        df2.SetName("Norm")
+        df2.SetDoubleAttribute("mean", 7.0)
+        df2.SetDoubleAttribute("sd", 2)
+        
+        var1 = vtkXMLDataElement()
+        var1.SetName("Variable")
+        var1.SetAttribute("name", "nmin")
+        var1.SetAttribute("type", "norm")
+        var1.AddNestedElement(df1)
+        
+        var2 = vtkXMLDataElement()
+        var2.SetName("Variable")
+        var2.SetAttribute("name", "pH")
+        var2.SetAttribute("type", "norm")
+        var2.AddNestedElement(df2)
+        
+        root  = vtk.vtkXMLDataElement()
+        root.SetName("DataDistributionDescription")
+        root.SetAttribute("xmlns", "http://tag2e.googlecode.com/files/DataDistributionDescription")
+        root.SetAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
+        root.SetAttribute("xsi:schemaLocation", "http://tag2e.googlecode.com/files/DataDistributionDescription http://tag2e.googlecode.com/files/DataDistributionDescription.xsd")
+        root.SetAttribute("name", "Test2")
+        root.AddNestedElement(var1)
+        root.AddNestedElement(var2)
+        
+        self.ddd.SetFileName("/tmp/MCWFISSimpleDataDistributionDescription1.xml")
+        self.ddd.GetXMLRoot().DeepCopy(root)
+        self.ddd.Write()
+                
+        analyser = vtkTAG2EModelMonteCarloVariationAnalyser()
+        analyser.SetDataDistributionDescription(self.ddd)
+        analyser.SetModel(self.Model)
+        analyser.SetNumberOfRandomValues(1000)
+        analyser.SetNumberOfTimeSteps(5)
+        analyser.SetMaxNumberOfIterations(2000)
+        analyser.SetBreakCriterion(0.001)
+        analyser.Update()
+        
+        output = analyser.GetOutput()
+        
+        # Generate the output        
+        num = output.GetNumberOfTimeSteps()
+        
+        writer = vtkXMLPolyDataWriter()
+        
+        for i in range(1):
+            writer.SetFileName("/tmp/TAG2EMonteCarloWFISTest_1_" + str(i) + ".vtp")
+            writer.SetInput(output.GetTimeStep(i))
+            writer.Write()
+            
+        print output.GetFieldData().GetArray(self.Model.GetResultArrayName())
+        print output.GetFieldData().GetArray(self.Model.GetResultArrayName()).GetRange()
+        
+        # make a statistical anlysis possible using R
+        self.riface.AssignVTKDataArrayToRVariable(output.GetFieldData().GetArray(self.Model.GetResultArrayName()), self.Model.GetResultArrayName())
+        
+        # Save the workspace for testing
+        script = "save(list = ls(all=TRUE), file = \"/home/soeren/MonteCarloTestWFIS\")"
         print script
         self.riface.EvalRscript(script, True)
         
@@ -385,6 +594,9 @@ if __name__ == '__main__':
     unittest.TextTestRunner(verbosity=2).run(suite1) 
     suite2 = unittest.TestLoader().loadTestsFromTestCase(vtkTAG2EModelMonteCarloVariationAnalyserTestsComplex)
     unittest.TextTestRunner(verbosity=2).run(suite2) 
+    suite3 = unittest.TestLoader().loadTestsFromTestCase(vtkTAG2EModelMonteCarloVariationAnalyserTestsWFIS)
+    unittest.TextTestRunner(verbosity=2).run(suite3) 
+
 
     
     
--- trunk/Filtering/vtkTAG2EWeightedFuzzyInferenceModel.h
+++ trunk/Filtering/vtkTAG2EWeightedFuzzyInferenceModel.h
@@ -31,8 +31,10 @@
  */
 
 /**
- * \brief 
- * 
+ * \brief This class uses a weighted fuzzy inference scheme to process
+ * the point data of the temporal input data sets. The weighted fuzzy inference 
+ * must be provided as as XML model parameter representation which is of type
+ * vtkTAG2EFuzzyInferenceModelParameter. 
  */
 
 #ifndef vtkTAG2EWeightedFuzzyInferenceModel_H
@@ -54,6 +56,9 @@ public:
      
     virtual double GetModelAssessmentFactor(){;}
     
+    //!\brief Set the model parameter which must be of type vtkTAG2EFuzzyInferenceModelParameter
+    //! This XML model parameter describes the fuzzy inference scheme which is used to compute 
+    //! the input point data.
     void SetModelParameter(vtkTAG2EAbstractModelParameter* modelParameter);
     
     // Verify the FIS comutation with simple test cases. No inputs required.
@@ -63,11 +68,33 @@ protected:
     vtkTAG2EWeightedFuzzyInferenceModel();
     ~vtkTAG2EWeightedFuzzyInferenceModel();
     
-    virtual bool ComputeRuleCodeMatrixEntries(std::vector< std::vector<int> > &RuleCodeArray, 
+    //!\brief This method computes the rule code matrix which is used to compute the 
+    //! the deegree of membership of an input factor for each rule
+    //!\param RuleCodeMatrix The matrix of coded rules
+    //!\param numberOfRules Number of rules
+    //!\param WFIS The internal representation of the weighted fuzzy inference scheme
+    //!\return true in case of success
+
+    virtual bool ComputeRuleCodeMatrixEntries(std::vector< std::vector<int> > &RuleCodeMatrix, 
                          int numberOfRules, WeightedFuzzyInferenceScheme &WFIS);
-    virtual double ComputeDOF(double *Input, int rule, std::vector< std::vector<int> > &RuleCodeArray, WeightedFuzzyInferenceScheme &WFIS);
-    virtual double ComputeFISResult(double *Input, int numberOfRules, std::vector< std::vector<int> > &RuleCodeArray, WeightedFuzzyInferenceScheme &WFIS);
-    virtual double InterpolatePointInNormDist (double value);
+    //!\brief Compute the deegree of fullfillment of a single point for a single rule
+    //!\param The factor input vector of a single point at a single time step
+    //!\param rule The index of a single rule (index of row in the RuleCodeMatrix)
+    //!\param RuleCodeMatrix The matrix of coded rules
+    //!\param WFIS The internal representation of the weighted fuzzy inference scheme
+    //!\return The deegree of fullfillment of a single rule
+    virtual double ComputeDOF(double *Input, int rule, std::vector< std::vector<int> > &RuleCodeMatrix, WeightedFuzzyInferenceScheme &WFIS);
+
+    //!\brief Compute the weighted fuzzy inference scheme result for a single point  
+    //!\param The factor input vector of a single point at a single time step
+    //!\param numberOfRules Number of rules
+    //!\param RuleCodeMatrix The matrix of coded rules
+    //!\param WFIS The internal representation of the weighted fuzzy inference scheme
+    //!\return The result of the fuzzy inference scheme computation
+    virtual double ComputeFISResult(double *Input, int numberOfRules, std::vector< std::vector<int> > &RuleCodeMatrix, WeightedFuzzyInferenceScheme &WFIS);
+
+   //!\brief Interpolate the y value of a standard normal distribution and position x
+    virtual double InterpolatePointInNormDist (double x);
     
     virtual int RequestData(vtkInformation *, vtkInformationVector **, vtkInformationVector *);
     virtual int FillInputPortInformation(int port, vtkInformation* info);
