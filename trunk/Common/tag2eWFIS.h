d0be3a67ec9e6a128b65fd4e177f4ada337731f6
soerengebbert <soerengebbert@16fbe261-6e97-5eb1-f61f-830664b603fa>
2011-04-14 17:16:33 +0000

    Moved weighted fuzzy inference code into a separate class

---
+++ trunk/Common/tag2eWFIS.h
@@ -0,0 +1,128 @@
+#ifndef TAG2EWFIS_H
+#define TAG2EWFIS_H
+
+#include <vector>
+
+#define FUZZY_SET_POISITION_LEFT 0
+#define FUZZY_SET_POISITION_INT 1
+#define FUZZY_SET_POISITION_RIGHT 2
+#define FUZZY_SET_TYPE_TRIANGULAR 0
+#define FUZZY_SET_TYPE_CRISP 1
+#define FUZZY_SET_TYPE_BELL_SHAPE 2
+
+// The documentation of the following trivial classes
+// is located in the WeightedFuzzyInferenceScheme.xsd
+class FuzzyShapeTriangular{
+public:
+    double center;
+    double left;
+    double right;
+};
+
+class FuzzyShapeCrisp{
+public:
+    double left;
+    double right;
+};
+
+class FuzzyShapeBell{
+public:
+    double center;
+    double sdLeft;
+    double sdRight;
+};
+
+class FuzzySet {
+public:
+    unsigned int type;
+    unsigned int priority;
+    unsigned int position;
+    bool constant;
+    FuzzyShapeTriangular Triangular;
+    FuzzyShapeCrisp Crisp;
+    FuzzyShapeBell BellShape;
+};
+
+class FuzzyFactor {
+public:
+    int portId;
+    std::string name;
+    double min;
+    double max;
+    std::vector<FuzzySet> Sets;
+};
+
+class FuzzyResponse{
+public:
+    bool constant;
+    double value;
+    double sd;
+};
+
+class FuzzyResponses{
+public:
+    double min;
+    double max;
+    std::vector<FuzzyResponse> Responses;
+};
+
+class FuzzyInferenceScheme {
+public:
+    std::vector<FuzzyFactor> Factors;
+    FuzzyResponses Responses;
+};
+
+class FuzzyWeight{
+public:
+    std::string name;
+    bool active;
+    bool constant;
+    double value;
+    double min;
+    double max;
+};
+
+class WeightedFuzzyInferenceScheme {
+public:
+    std::string name;
+    FuzzyInferenceScheme FIS;
+    FuzzyWeight Weight;
+};
+
+/** 
+ * This class contains the computation algorithms of the weighted fuzzy inference scheme
+ * 
+ */
+class tag2eWFIS {
+public:
+   
+    //!\brief This method computes the rule code matrix which is used to compute the 
+    //! the deegree of membership of an input factor for each rule
+    //!\param RuleCodeMatrix The matrix of coded rules
+    //!\param numberOfRules Number of rules
+    //!\param WFIS The internal representation of the weighted fuzzy inference scheme
+    //!\return true in case of success
+    static bool ComputeRuleCodeMatrixEntries(std::vector< std::vector<int> > &RuleCodeMatrix, 
+                         int numberOfRules, WeightedFuzzyInferenceScheme &WFIS);
+    //!\brief Compute the deegree of fullfillment of a single point for a single rule
+    //!\param The factor input vector of a single point at a single time step
+    //!\param rule The index of a single rule (index of row in the RuleCodeMatrix)
+    //!\param RuleCodeMatrix The matrix of coded rules
+    //!\param WFIS The internal representation of the weighted fuzzy inference scheme
+    //!\return The deegree of fullfillment of a single rule
+    static double ComputeDOF(double *Input, int rule, std::vector< std::vector<int> > &RuleCodeMatrix, WeightedFuzzyInferenceScheme &WFIS);
+
+    //!\brief Compute the weighted fuzzy inference scheme result for a single point  
+    //!\param The factor input vector of a single point at a single time step
+    //!\param numberOfRules Number of rules
+    //!\param RuleCodeMatrix The matrix of coded rules
+    //!\param WFIS The internal representation of the weighted fuzzy inference scheme
+    //!\return The result of the fuzzy inference scheme computation
+    static double ComputeFISResult(double *Input, int numberOfRules, std::vector< std::vector<int> > &RuleCodeMatrix, WeightedFuzzyInferenceScheme &WFIS);
+
+   //!\brief Interpolate the y value of a standard normal distribution and position x
+    static double InterpolatePointInNormDist (double x);
+
+};
+
+#endif	/* TAG2EWFIS_H */
--- trunk/Filtering/vtkTAG2EWeightedFuzzyInferenceModel.cxx
+++ trunk/Filtering/vtkTAG2EWeightedFuzzyInferenceModel.cxx
@@ -47,44 +47,7 @@
 #include "vtkTAG2EWeightedFuzzyInferenceModel.h"
 #include "vtkTAG2EFuzzyInferenceModelParameter.h"
 
-#define TOLERANCE 0.000000001
-
-/* This is a static array with smapling points and associated values
- * of the standard normal distribution. It is
- * used for faster computation.
- */
-static double NormDistSamplingPoints[27][2] = {
-  {-4, 0.000335463},
-  {-3, 0.011108997},
-  {-2.5, 0.043936934},
-  {-2, 0.135335283},
-  {-1.8, 0.197898699},
-  {-1.6, 0.2780373},
-  {-1.4, 0.375311099},
-  {-1.2, 0.486752256},
-  {-1, 0.60653066},
-  {-0.8, 0.726149037},
-  {-0.6, 0.835270211},
-  {-0.4, 0.923116346},
-  {-0.2, 0.980198673},
-  {0, 1},
-  {0.2, 0.980198673},
-  {0.4, 0.923116346},
-  {0.6, 0.835270211},
-  {0.8, 0.726149037},
-  {1, 0.60653066},
-  {1.2, 0.486752256},
-  {1.4, 0.375311099},
-  {1.6, 0.2780373},
-  {1.8, 0.197898699},
-  {2, 0.135335283},
-  {2.5, 0.043936934},
-  {3, 0.011108997},
-  {4, 0.000335463}
-};
-
-// Simple maximum computation
-static double max(double x, double y);
+#include "tag2eWFIS.h"
 
 vtkCxxRevisionMacro(vtkTAG2EWeightedFuzzyInferenceModel, "$Revision: 1.0 $");
 vtkStandardNewMacro(vtkTAG2EWeightedFuzzyInferenceModel);
@@ -219,7 +182,7 @@ int vtkTAG2EWeightedFuzzyInferenceModel::RequestData(
   std::vector< std::vector<int> > RuleCodeMatrix(numberOfRules, std::vector<int>(numberOfFactors));
 
   // Compute the rule code matrix entries 
-  this->ComputeRuleCodeMatrixEntries(RuleCodeMatrix, numberOfRules, WFIS);
+  tag2eWFIS::ComputeRuleCodeMatrixEntries(RuleCodeMatrix, numberOfRules, WFIS);
 
   // get the first input and ouptut
   vtkTemporalDataSet *firstInput = vtkTemporalDataSet::SafeDownCast(
@@ -312,7 +275,7 @@ int vtkTAG2EWeightedFuzzyInferenceModel::RequestData(
         Data[j]->GetTuple(i, &fuzzyInput[j]);
       }
 
-      double val = this->ComputeFISResult(fuzzyInput, numberOfRules, RuleCodeMatrix, WFIS);
+      double val = tag2eWFIS::ComputeFISResult(fuzzyInput, numberOfRules, RuleCodeMatrix, WFIS);
       result->SetValue(i, val);
       delete [] fuzzyInput;
     }
@@ -335,221 +298,10 @@ void vtkTAG2EWeightedFuzzyInferenceModel::PrintSelf(ostream& os, vtkIndent inden
   this->Superclass::PrintSelf(os, indent);
 }
 
-//----------------------------------------------------------------------------
-
-bool vtkTAG2EWeightedFuzzyInferenceModel::ComputeRuleCodeMatrixEntries(std::vector< std::vector<int> > &RuleCodeMatrix, int numberOfRules, WeightedFuzzyInferenceScheme &WFIS)
-{
-  int col, row, length, length1, dum, inp, num, num1;
-  int numberOfFactors = WFIS.FIS.Factors.size();
-
-  // Compute the rule code matrix which contains the 
-  // permutation of all factors. The matrix enries are
-  // the ids of the fuzzy set shapes.
-  // The size of the matrix is numberOfRules * numberOfFactors.
-  // Example:
-  //
-  // 2 factors with each 2 fuzzy sets == 4 Rules  
-  //
-  // The resulting matrix is of form:
-  // 0 0
-  // 0 1
-  // 1 0
-  // 1 1
-
-  for (col = numberOfFactors - 1; col >= 0; col--) {
-    length = 1;
-    dum = col + 1;
-    while (dum <= numberOfFactors - 1) {
-      FuzzyFactor &Factor = WFIS.FIS.Factors[dum];
-      length = length * Factor.Sets.size();
-      dum = dum + 1;
-    }
-    length1 = 1;
-    dum = col;
-    while (dum <= numberOfFactors - 1) {
-      FuzzyFactor &Factor = WFIS.FIS.Factors[dum];
-      length1 = length1 * Factor.Sets.size();
-      dum = dum + 1;
-    }
-    inp = 0;
-    num = 0;
-    num1 = 0;
-    for (row = 0; row < numberOfRules; row++) {
-      RuleCodeMatrix [row][col] = inp;
-      num = num + 1;
-      num1 = num1 + 1;
-      if (num == length) {
-        num = 0;
-        inp = inp + 1;
-      }
-      if (num1 == length1) {
-        num = 0;
-        inp = 0;
-        num1 = 0;
-      }
-    }
-  }
-
-  return true;
-}
 
 //----------------------------------------------------------------------------
 
-double vtkTAG2EWeightedFuzzyInferenceModel::ComputeFISResult(double *Input,
-  int numberOfRules, std::vector< std::vector<int> > &RuleCodeMatrix, WeightedFuzzyInferenceScheme &WFIS)
-{
-  int rule;
-  double result;
-  double dof, sum_dofs;
-
-  sum_dofs = 0.0;
-  result = 0.0;
-
-  // Compute the deegree of fullfillment for each rule
-  for (rule = 0; rule < numberOfRules; rule++) {
-
-    dof = this->ComputeDOF(Input, rule, RuleCodeMatrix, WFIS);
-
-    sum_dofs = sum_dofs + dof;
-    result = result + WFIS.FIS.Responses.Responses[rule].value * dof;
-  }
-
-  // Check for wrong results and apply the deegrees of fullfillment
-  if (sum_dofs == 0) {
-    vtkErrorMacro( << "Sum of deegrees of fullfillments is 0. Expect wrong model results.");
-    result = 0.0;
-  } else {
-    result = result / sum_dofs;
-  }
-
-  // Weight the result
-  if (WFIS.Weight.active) {
-    result *= WFIS.Weight.value;
-  }
-
-  return result;
-}
-
-//----------------------------------------------------------------------------
-
-double vtkTAG2EWeightedFuzzyInferenceModel::ComputeDOF(double *Input,
-  int rule, std::vector< std::vector<int> > &RuleCodeMatrix, WeightedFuzzyInferenceScheme &WFIS)
-{
-  int numberOfFactors = WFIS.FIS.Factors.size();
-  int d, pos, shape;
-  double *dom = new double[numberOfFactors]; // Deegree of membership of a fuzzy set
-  double *normedInput = new double[numberOfFactors]; // Normalized input values
-  double dof; // The resulting deegree of fullfillment for the rule
-
-  //cout << "Normed values" << endl;
-  // Norm the Input values and write the result in normedInput
-  for (d = 0; d < numberOfFactors; d++) {
-    normedInput[d] = (Input[d] - WFIS.FIS.Factors[d].min) / (WFIS.FIS.Factors[d].max - WFIS.FIS.Factors[d].min);
-    //cout << normedInput[d] << endl;
-  }
-
-  d = 0; // counter
-  dof = 1; // We need o initialize the resulting DOF with 1
-  do {
-    pos = (RuleCodeMatrix[rule][d]);
-    shape = WFIS.FIS.Factors[d].Sets[pos].type;
-
-    // The value of normedInput at position d is normed [0:1] using its defiend max and min values. 
-    // Larger values of 1 are possible in case of the maximum was to low estimated.
-    // In case larger values than 10 we assume 
-    // the membership is 100%. Values larger than 10 may occure in case values are missing and 
-    // replaced by a large default number.
-    if ((normedInput[d] > 10) || (normedInput[d] < 0)) {
-      dom[d] = 1;
-    } else {
-      FuzzySet &Set = WFIS.FIS.Factors[d].Sets[pos];
-      switch (shape) {
-
-      case FUZZY_SET_TYPE_BELL_SHAPE: // gauss bell shape fuzzy numbers
-        if (normedInput[d] - Set.BellShape.center <= 0) //left_side of the mean of the fuzzy number
-        {
-          if ((normedInput[d] - Set.BellShape.center) / Set.BellShape.sdLeft<-4) {
-            dom[d] = 0.0;
-          } else {
-            dom[d] = InterpolatePointInNormDist((normedInput[d] - Set.BellShape.center) / Set.BellShape.sdLeft);
-          }
-        } else { //right side of the mean of the fuzzy number
-          if ((normedInput[d] - Set.BellShape.center) / Set.BellShape.sdRight > 4) {
-            dom[d] = 0.0;
-          } else {
-            dom[d] = InterpolatePointInNormDist((normedInput[d] - Set.BellShape.center) / Set.BellShape.sdRight);
-          }
-        }
-        break;
-
-      case FUZZY_SET_TYPE_TRIANGULAR: // triangular fuzzy numbers
-        double m;
-
-        if (normedInput[d] - Set.Triangular.center <= 0) //left side of the fuzzy number
-        {
-          if ((Set.Triangular.left) > 1111) {
-            dom[d] = 1.0;
-          } else {
-            m = 1.0 / Set.Triangular.left;
-            dom[d] = max(0.0, m * (normedInput[d] - Set.Triangular.center) + 1);
-          }
-        } else//right side of fuzzy number
-        {
-          if (Set.Triangular.right > 1111) {
-            dom[d] = 1.0;
-          } else {
-            m = -1.0 / Set.Triangular.right;
-            dom[d] = max(0.0, m * (normedInput[d] - Set.Triangular.center) + 1);
-          }
-        }
-        break;
-
-      case FUZZY_SET_TYPE_CRISP: // all or nothing the split is just in the middle between two alphas
-        if (normedInput[d] >= Set.Crisp.left && normedInput[d] <= Set.Crisp.right) {
-          dom[d] = 1.0;
-        } else {
-          dom[d] = 0.0;
-        }
-        break;
-      }
-    }
-
-    dof *= dom[d]; // Accumulate the deegree of fullfillment
-    d++;
-  } while (d < (numberOfFactors));
-
-  delete [] dom;
-  delete [] normedInput;
-  
-  //  cout << "Deegree of fullfillment " << dof << endl;
-  return dof;
-}
-
-//----------------------------------------------------------------------------
-
-double vtkTAG2EWeightedFuzzyInferenceModel::InterpolatePointInNormDist(double value)
-{
-  int w1 = 0, w2 = 26, w3 = 0;
-  double dummy;
-  do {
-    dummy = (w1 + w2) / 2;
-    w3 = floor(dummy);
-    if (value > NormDistSamplingPoints[w3 ][0]) {
-      w1 = w3;
-    } else {
-      w2 = w3;
-    }
-  } while (w1 + 1 < w2);
-
-  return NormDistSamplingPoints[w1][1] +
-    (NormDistSamplingPoints[w2][1] -
-    NormDistSamplingPoints[w1][1]) /
-    (NormDistSamplingPoints[w2][0] -
-    NormDistSamplingPoints[w1][0]) *
-    (value - NormDistSamplingPoints[w1][0]);
-}
-
-//----------------------------------------------------------------------------
+#define TOLERANCE 0.000000001
 
 bool vtkTAG2EWeightedFuzzyInferenceModel::TestFISComputation()
 {
@@ -650,7 +402,7 @@ bool vtkTAG2EWeightedFuzzyInferenceModel::TestFISComputation()
   // Create the rule code matrix
   std::vector< std::vector<int> > RuleCodeMatrix(numberOfRules, std::vector<int>(numberOfFactors));
 
-  this->ComputeRuleCodeMatrixEntries(RuleCodeMatrix, numberOfRules, WFIS);
+  tag2eWFIS::ComputeRuleCodeMatrixEntries(RuleCodeMatrix, numberOfRules, WFIS);
 
   for (i = 0; i < numberOfRules; i++) {
     for (j = 0; j < numberOfFactors; j++) {
@@ -667,7 +419,7 @@ bool vtkTAG2EWeightedFuzzyInferenceModel::TestFISComputation()
     RuleCodeMatrix[2][1] != 0 ||
     RuleCodeMatrix[3][0] != 1 ||
     RuleCodeMatrix[3][1] != 1) {
-    vtkErrorMacro( << "ComputeRuleCodeMatrixEntries failed");
+    (std::cerr << "ComputeRuleCodeMatrixEntries failed");
     return false;
   }
 
@@ -676,32 +428,32 @@ bool vtkTAG2EWeightedFuzzyInferenceModel::TestFISComputation()
   double Input[2] = {75.0, 10.0};
   double result;
 
-  result = ComputeDOF(Input, 0, RuleCodeMatrix, WFIS);
+  result = tag2eWFIS::ComputeDOF(Input, 0, RuleCodeMatrix, WFIS);
   if (fabs(result - 0.25) > TOLERANCE) {
-    vtkErrorMacro( << "Wrong result in ComputeDOF Test 1 1");
+    (std::cerr << "Wrong result in ComputeDOF Test 1 1");
     return false;
   }
-  result = ComputeDOF(Input, 1, RuleCodeMatrix, WFIS);
+  result = tag2eWFIS::ComputeDOF(Input, 1, RuleCodeMatrix, WFIS);
   if (fabs(result - 0.25) > TOLERANCE) {
-    vtkErrorMacro( << "Wrong result in ComputeDOF Test 1 2");
+    (std::cerr << "Wrong result in ComputeDOF Test 1 2");
     return false;
   }
-  result = ComputeDOF(Input, 2, RuleCodeMatrix, WFIS);
+  result = tag2eWFIS::ComputeDOF(Input, 2, RuleCodeMatrix, WFIS);
   if (fabs(result - 0.25) > TOLERANCE) {
-    vtkErrorMacro( << "Wrong result in ComputeDOF Test 1 3");
+    (std::cerr << "Wrong result in ComputeDOF Test 1 3");
     return false;
   }
-  result = ComputeDOF(Input, 3, RuleCodeMatrix, WFIS);
+  result = tag2eWFIS::ComputeDOF(Input, 3, RuleCodeMatrix, WFIS);
   if (fabs(result - 0.25) > TOLERANCE) {
-    vtkErrorMacro( << "Wrong result in ComputeDOF Test 1 4");
+    (std::cerr << "Wrong result in ComputeDOF Test 1 4");
     return false;
   }
 
   cout << "ComputeFISResult Test 1" << endl;
-  result = ComputeFISResult(Input, numberOfRules, RuleCodeMatrix, WFIS);
+  result = tag2eWFIS::ComputeFISResult(Input, numberOfRules, RuleCodeMatrix, WFIS);
   cout << "Result = " << result << endl;
   if (fabs(result - 2.5) > TOLERANCE) {
-    vtkErrorMacro( << "Wrong result in ComputeFISResult Test 1");
+    (std::cerr << "Wrong result in ComputeFISResult Test 1");
   }
 
   cout << "ComputeDOF Test 2 Left border" << endl;
@@ -709,28 +461,28 @@ bool vtkTAG2EWeightedFuzzyInferenceModel::TestFISComputation()
   Input[0] = 0.0;
   Input[1] = -15.0;
 
-  result = ComputeDOF(Input, 0, RuleCodeMatrix, WFIS);
+  result = tag2eWFIS::ComputeDOF(Input, 0, RuleCodeMatrix, WFIS);
   if (fabs(result - 1.0) > TOLERANCE) {
-    vtkErrorMacro( << "Wrong result in ComputeDOF Test 2 1");
+    (std::cerr << "Wrong result in ComputeDOF Test 2 1");
   }
-  result = ComputeDOF(Input, 1, RuleCodeMatrix, WFIS);
+  result = tag2eWFIS::ComputeDOF(Input, 1, RuleCodeMatrix, WFIS);
   if (fabs(result - 0.0) > TOLERANCE) {
-    vtkErrorMacro( << "Wrong result in ComputeDOF Test 2 2");
+    (std::cerr << "Wrong result in ComputeDOF Test 2 2");
   }
-  result = ComputeDOF(Input, 2, RuleCodeMatrix, WFIS);
+  result = tag2eWFIS::ComputeDOF(Input, 2, RuleCodeMatrix, WFIS);
   if (fabs(result - 0.0) > TOLERANCE) {
-    vtkErrorMacro( << "Wrong result in ComputeDOF Test 2 3");
+    (std::cerr << "Wrong result in ComputeDOF Test 2 3");
   }
-  result = ComputeDOF(Input, 3, RuleCodeMatrix, WFIS);
+  result = tag2eWFIS::ComputeDOF(Input, 3, RuleCodeMatrix, WFIS);
   if (fabs(result - 0.0) > TOLERANCE) {
-    vtkErrorMacro( << "Wrong result in ComputeDOF Test 2 4");
+    (std::cerr << "Wrong result in ComputeDOF Test 2 4");
   }
 
   cout << "ComputeFISResult Test 2" << endl;
-  result = ComputeFISResult(Input, numberOfRules, RuleCodeMatrix, WFIS);
+  result = tag2eWFIS::ComputeFISResult(Input, numberOfRules, RuleCodeMatrix, WFIS);
   cout << "Result = " << result << endl;
   if (fabs(result - 1.0) > TOLERANCE) {
-    vtkErrorMacro( << "Wrong result in ComputeFISResult Test 2");
+    (std::cerr << "Wrong result in ComputeFISResult Test 2");
   }
 
   cout << "ComputeDOF Test 3 right border" << endl;
@@ -738,42 +490,29 @@ bool vtkTAG2EWeightedFuzzyInferenceModel::TestFISComputation()
   Input[0] = 150;
   Input[1] = 35;
 
-  result = ComputeDOF(Input, 0, RuleCodeMatrix, WFIS);
+  result = tag2eWFIS::ComputeDOF(Input, 0, RuleCodeMatrix, WFIS);
   if (fabs(result - 0.0) > TOLERANCE) {
-    vtkErrorMacro( << "Wrong result in ComputeDOF Test 3 1");
+    (std::cerr << "Wrong result in ComputeDOF Test 3 1");
   }
-  result = ComputeDOF(Input, 1, RuleCodeMatrix, WFIS);
+  result = tag2eWFIS::ComputeDOF(Input, 1, RuleCodeMatrix, WFIS);
   if (fabs(result - 0.0) > TOLERANCE) {
-    vtkErrorMacro( << "Wrong result in ComputeDOF Test 3 2");
+    (std::cerr << "Wrong result in ComputeDOF Test 3 2");
   }
-  result = ComputeDOF(Input, 2, RuleCodeMatrix, WFIS);
+  result = tag2eWFIS::ComputeDOF(Input, 2, RuleCodeMatrix, WFIS);
   if (fabs(result - 0.0) > TOLERANCE) {
-    vtkErrorMacro( << "Wrong result in ComputeDOF Test 3 3");
+    (std::cerr << "Wrong result in ComputeDOF Test 3 3");
   }
-  result = ComputeDOF(Input, 3, RuleCodeMatrix, WFIS);
+  result = tag2eWFIS::ComputeDOF(Input, 3, RuleCodeMatrix, WFIS);
   if (fabs(result - 1.0) > TOLERANCE) {
-    vtkErrorMacro( << "Wrong result in ComputeDOF Test 3 4");
+    (std::cerr << "Wrong result in ComputeDOF Test 3 4");
   }
 
   cout << "ComputeFISResult Test 3" << endl;
-  result = ComputeFISResult(Input, numberOfRules, RuleCodeMatrix, WFIS);
+  result = tag2eWFIS::ComputeFISResult(Input, numberOfRules, RuleCodeMatrix, WFIS);
   cout << "Result = " << result << endl;
   if (fabs(result - 4.0) > TOLERANCE) {
-    vtkErrorMacro( << "Wrong result in ComputeFISResult Test 3");
+    (std::cerr << "Wrong result in ComputeFISResult Test 3");
   }
 
   return true;
-}
-
-//----------------------------------------------------------------------------
-
-double max(double x, double y)
-{
-  double w;
-  if (x < y) {
-    w = y;
-  } else {
-    w = x;
-  }
-  return w;
 }
