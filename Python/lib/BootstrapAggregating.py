bfda4474e5a224e5f887391c71a62b8187f282d6
mechthild.berthold <mechthild.berthold@gmail.com>
2012-08-27 11:40:30 +0000

    Modified bootstrap aggregation to consider type arrays

--- Python/lib/BootstrapAggregating.py
+++ Python/lib/BootstrapAggregating.py
@@ -39,35 +39,36 @@ from libvtkTAG2EFilteringPython import *
 ################################################################################
 ################################################################################
 
-def CellSampling(dataset):
+def CellSampling(dataset, typeArray=None):
 
-    return Sampling(dataset, "cell")
-
-################################################################################
-################################################################################
-################################################################################
-
-def PointSampling(dataset):
-
-    return Sampling(dataset, "point")
+    return Sampling(dataset, "cell", typeArray)
 
 ################################################################################
 ################################################################################
 ################################################################################
 
-def Sampling(dataset, type):
+def PointSampling(dataset, typeArray=None):
+
+    return Sampling(dataset, "point", typeArray)
+
+################################################################################
+################################################################################
+################################################################################
+
+def Sampling(dataset, type_, typeArray=None):
 
     selection = vtkSelection()
     selectionNode = vtkSelectionNode()
     extract = vtkExtractSelectedPolyDataIds()
 
     selectionNode.GetProperties().Set(vtkSelectionNode.CONTENT_TYPE(), vtkSelectionNode.INDICES)
-    if type == "point":
+    if type_ == "point":
         selectionNode.GetProperties().Set(vtkSelectionNode.FIELD_TYPE(), vtkSelectionNode.POINT)
     else:
+        print "CELLS"
         selectionNode.GetProperties().Set(vtkSelectionNode.FIELD_TYPE(), vtkSelectionNode.CELL)
 
-    if type == "point":
+    if type_ == "point":
         num = dataset.GetNumberOfPoints()
     else:
         num = dataset.GetNumberOfCells()
@@ -75,10 +76,50 @@ def Sampling(dataset, type):
     ids = vtkIdTypeArray()
     ids.SetName("Selection")
     ids.SetNumberOfTuples(num)
-    
-    for i in range(num):
-        id = random.randint(0, num - 1)
-        ids.SetTuple1(i, id)
+
+    # In case the typeArray is set, the sampling probability is weighted by the 
+		# inverse of the number of data in each type 
+    # --> each type is sampled with the same probability
+    if typeArray:
+        types = {}
+        # Get the array with the numerical types
+        if type_ == "point":
+            array = dataset.GetPointData().GetArray(typeArray)
+        else:
+            array = dataset.GetCellData().GetArray(typeArray)
+            if not array:
+                raise IOError("Type array not found in input dataset")
+        # We extract for each type the cell ids and save this in a nested dict-list
+        for i in range(num):
+            id_ = int(array.GetTuple1(i))
+            if id_ in types:
+                types[id_].append(i)
+            else:
+                types[id_] = []
+                types[id_].append(i)
+        print types
+        # Store the keys in a list for index access
+        key_list = []
+        for key in types.keys():
+            key_list.append(key)
+        # Select the cell ids randomly
+        for i in range(num):
+            num2 = len(key_list)
+            # Select the type specific cell id array randomly
+            key_id = random.randint(0, num2 - 1)
+            print "Select type: ", key_list[key_id]
+            num2 = len(types[key_list[key_id]])
+            # Select a cell id from the type array randomly
+            cell_id = random.randint(0, num2 - 1)
+            id_ = types[key_list[key_id]][cell_id]
+            print "Selected cell/point id: ", id_
+            # Save the id to sellect the cells from the input dataset
+            ids.SetTuple1(i, id_)
+    else:
+        for i in range(num):
+            id_ = random.randint(0, num - 1)
+            print id_
+            ids.SetTuple1(i, id_)
 
     selectionNode.SetSelectionList(ids)
     selection.AddNode(selectionNode)
@@ -86,6 +127,8 @@ def Sampling(dataset, type):
     extract.SetInput(0, dataset)
     extract.SetInput(1, selection)
     extract.Update()
+
+    print extract.GetOutput()
 
     if extract.GetOutput().GetNumberOfPoints() != dataset.GetNumberOfPoints():
         raise IOError("Error while bagging, number of points are different")
---
+++ Python/lib/BootstrapAggregatingTest.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+#
+# Toolkit for Agriculture Greenhouse Gas Emission Estimation TAG2E
+#
+# Authors: Soeren Gebbert, soeren.gebbert@vti.bund.de
+#          Rene Dechow, rene.dechow@vti.bund.de
+#
+# Copyright:
+#
+# Johann Heinrich von Thuenen-Institut
+# Institut fuer Agrarrelevante Klimaforschung
+#
+# Phone: +49 (0)531 596 2601
+#
+# Fax:+49 (0)531 596 2699
+#
+# Mail: ak@vti.bund.de
+#
+# Bundesallee 50
+# 38116 Braunschweig
+# Germany
+#
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; version 2 of the License.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#include the VTK and vtkGRASSBridge Python libraries
+import unittest
+
+from vtk import *
+
+from libvtkTAG2ECommonPython import *
+from libvtkTAG2EFilteringPython import *
+from libvtkGRASSBridgeTemporalPython import *
+from libvtkGRASSBridgeCommonPython import *
+import BootstrapAggregating as ba 
+
+class BootstrapAggregationTest(unittest.TestCase):
+  
+    def setUp(self):
+        
+        # Create the point data
+        xext = 5
+        yext = 4
+        num = xext*yext
+                
+        pH = vtkDoubleArray()
+        pH.SetNumberOfTuples(num)
+        pH.SetName("pH")
+        
+        type_ = vtkIntArray()
+        type_.SetNumberOfTuples(num)
+        type_.SetName("type")
+        
+        # Point ids for poly vertex cell
+        points = vtkPoints()
+ 
+        self.ds1 = vtkPolyData()
+        self.ds1.Allocate(xext,yext)
+  
+        self.ds2 = vtkPolyData()
+        self.ds2.Allocate(xext,yext)
+      
+        count = 0
+        for i in range(xext):
+            for j in range(yext):
+                ids = vtkIdList()
+                id_ = points.InsertNextPoint(i + j, j, 0)
+                ids.InsertNextId(id_)
+                type_.SetTuple1(count, i)
+                pH.SetTuple1(count, count)
+                self.ds1.InsertNextCell(vtk.VTK_VERTEX, ids)
+                self.ds2.InsertNextCell(vtk.VTK_VERTEX, ids)
+                count += 1
+
+        self.ds1.GetCellData().SetScalars(pH)
+        self.ds1.GetCellData().AddArray(type_)
+        self.ds1.SetPoints(points)    
+
+        self.ds2.GetPointData().SetScalars(pH)
+        self.ds2.GetPointData().AddArray(type_)
+        self.ds2.SetPoints(points)    
+
+        writer = vtkPolyDataWriter()
+        writer.SetInput(self.ds1)
+        writer.SetFileName("/tmp/ba_1a.vtk")
+        writer.Write()
+ 
+        writer = vtkPolyDataWriter()
+        writer.SetInput(self.ds2)
+        writer.SetFileName("/tmp/ba_1b.vtk")
+        writer.Write()
+        
+       
+    def test1(self):
+
+      new_ds = ba.CellSampling(self.ds1)
+
+      writer = vtkPolyDataWriter()
+      writer.SetInput(new_ds)
+      writer.SetFileName("/tmp/ba_2a.vtk")
+      writer.Write()
+        
+    def otest2(self):
+
+      new_ds = ba.PointSampling(self.ds2)
+
+      writer = vtkPolyDataWriter()
+      writer.SetInput(new_ds)
+      writer.SetFileName("/tmp/ba_2b.vtk")
+      writer.Write()
+        
+    def test3(self):
+
+      new_ds = ba.CellSampling(self.ds1, "type")
+
+      writer = vtkPolyDataWriter()
+      writer.SetInput(new_ds)
+      writer.SetFileName("/tmp/ba_3a.vtk")
+      writer.Write()
+        
+ 
+
+
+            
+if __name__ == '__main__':
+    suite1 = unittest.TestLoader().loadTestsFromTestCase(BootstrapAggregationTest)
+    unittest.TextTestRunner(verbosity=2).run(suite1) 
